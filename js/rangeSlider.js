69f11709bc22e8808f85a570e3a0e1eb5e832f7b
bishwajit70 <mailtobishwa15@gmail.com>
2022-06-12 22:59:23 +0600

    final commit

---
+++ js/rangeSlider.js
@@ -0,0 +1,7 @@
+function rangeSlide1(value) {
+    document.getElementById('rangeValue1').innerHTML = value;
+}
+
+function rangeSlide2(value) {
+    document.getElementById('rangeValue2').innerHTML = value;
+}
